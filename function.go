a92029782e6b9bdd3a188bb2a41ffd10c0de8ac0
Diijon Taylor <diijon@gmail.com>
2019-10-05 11:56:54 -0400

    passing test

--- function.go
+++ function.go
@@ -13,13 +13,17 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Message string `json:"message"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
 		fmt.Fprint(w, "Hello World!")
+
 		return
 	}
+
 	if d.Message == "" {
 		fmt.Fprint(w, "Hello World!")
 		return
 	}
+
 	fmt.Fprint(w, html.EscapeString(d.Message))
 }
